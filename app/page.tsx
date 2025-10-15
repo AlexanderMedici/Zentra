e466b7f2adb2baba3f460244630aaa29e1a63fdc
Alexander M <hellomedici@outlook.com>
2025-10-15 12:57:16 -0400

    Zentra start

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Button} from"@/components/ui/button";
+
+
+
+const Home:()=> Element=()=> {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+    <Button>click me</Button>
+    </div>
+  )
+}
+
+export default Home
+
